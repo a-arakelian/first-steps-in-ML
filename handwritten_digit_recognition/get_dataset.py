c1839827db777ef043c0929a51fcf5b072404ffe
aram <arakeljan@gmail.com>
2022-11-25 23:32:23 +0400

    added some functions for getting dataset

--- handwritten_digit_recognition/get_dataset.py
+++ handwritten_digit_recognition/get_dataset.py
@@ -49,3 +49,32 @@ class DigitExample:
                 path = self.digits_file[digit][j]
                 print('The digit is ' + digit)
                 print(mpimg.imread(path))
+
+def digit2array(digit):
+    digit_array = np.zeros(10)
+    digit_array[digit] = 1
+    return digit_array
+
+def array2digit(array):
+    return array.argmax()
+
+def show_array_as_image(array):
+    plt.imshow(array)
+
+def from_dataset_jpgs2array(training_data = True, N = None):
+    folder_name = 'training' if training_data else 'testing'
+    folders = [
+        (hdr_directory + '/' + 'Dataset' + '/' + folder_name + '/' + str(digit), digit)
+        for digit in range(10)
+    ]
+    digit_file_names = {}
+    digit_file_names_len = []
+    for path in folders:
+        arr = os.listdir(path[0])
+        digit_file_names[path[1]] = [path[0] + '/' + i  for i in arr if i.endswith(".jpg")]
+        digit_file_names_len.append(len(digit_file_names[path[1]]))
+    if N == None: N = min(digit_file_names_len)
+    digit_arrays = {}
+    for i in range(10):
+        digit_arrays[i] = [mpimg.imread(path) for path in digit_file_names[i][:N]]
+    return digit_arrays
